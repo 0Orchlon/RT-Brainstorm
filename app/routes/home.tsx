aee42e13b4bde001b55ae2365c132e97002be195
Orchlon <sw22d045@mandakh.edu.mn>
2025-06-26 12:52:34 +0800

    installed google genai and ai api

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
-
+import aiResponse from "~/components/AIAPI";
+import { useState } from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +10,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  // State to hold the AI response
+  const [aiText, setAiText] = useState<string | null>(null);
+  
+  aiResponse.then((text) => {
+    console.log("AI says:", text);
+    setAiText(text);
+  });
+
+  return<div>
+<p>
+        {aiText ? aiText : "Loading AI response..."} {/* Render the response */}
+      </p>
+  <Welcome />
+  </div>
 }
